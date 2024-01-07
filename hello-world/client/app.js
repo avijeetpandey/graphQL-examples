01aeaaa575e912e0b5f8a11c8fe9d0045794e236
Avijeet pandey <avijeetpandey25@gmail.com>
2024-01-07 15:46:33 +0530

    Add: Sample graphql code

---
+++ hello-world/client/app.js
@@ -0,0 +1,18 @@
+async function getGreetings() {
+  const response = await fetch("http://localhost:9000", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify({
+      query: "query { greeting }",
+    }),
+  });
+  const body = await response.json();
+
+  // append the response on the frontend
+  const p = document.getElementById("response");
+  p.innerHTML = body?.data?.greeting;
+}
+
+getGreetings();
